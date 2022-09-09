0a74af8d39b4192d0157bcf3ea67a774f00bb49c
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-09-09 11:00:34 +0530

    this is udemy exercise.

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class ComplexNumber {
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary) {
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void  add(ComplexNumber second) {
+        double r = this.real + second.real;
+        double i = this.imaginary + second.imaginary;
+        this.real = r;
+        this.imaginary = i;
+    }
+
+    public void subtract(double real, double imaginary) {
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber second) {
+        double r = this.real - second.real;
+        double i = this.imaginary - second.imaginary;
+        this.real = r;
+        this.imaginary = i;
+    }
+
 }
